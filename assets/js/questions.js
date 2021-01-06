63a6dd528fa96aa0d500607d24ca942c50b067c2
Eric Nguyen <erictn3@uci.edu>
2021-01-06 11:02:35 -0800

    first draft

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "How many continents are there?",
+    choices: ["1", "3", "5", "7"],
+    answer: "7"
+  },
+  {
+    title: "Which city is the capital of Japan?",
+    choices: ["Shanghai", "Kyoto", "Beijing", "Tokyo"],
+    answer: "Tokyo"
+  },
+  {
+    title: "What is not an element of hip-hop culture?",
+    choices: ["deejaying","pop-lockin","rapping","graffiti"],
+    answer: "pop-lockin"
+  },
+];
